7be18d7537a5c9775d92eb52a379de75562bf045
ArmandoIsaacParraCruz <armandoisaac.parracruz@gmail.com>
2023-07-04 18:58:36 -0600

    Firts commit

---
+++ lib/HMI/HMI.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <Arduino.h>
+#include <vector>
+#include "GUI.h"
+#include "RemoteCommunication.h"
+#include "keyboard.h"
+
+#define NUMBER_OF_PLACES 6
+#define TASK 20
+
+enum class TemperatureSensorType{infrared, thermocouples};
+enum class TemperatureFunctionType{constant, ramp};
+
+struct TemperatureSetpoint {
+            uint16_t temperature;
+            TemperatureFunctionType tempFunction;
+};
+
+struct ExecutionSpecifications{
+    TemperatureSensorType sensor;
+    bool selectedPlaces[NUMBER_OF_PLACES];
+    TemperatureSetpoint temperatureSetpoint[TASK];
+    uint16_t stirringSetpoints[TASK]; 
+    uint32_t taskDuration[TASK];
+    uint8_t configuredTask;
+};
+
+class HMI
+{
+    public:
+        HMI();  
+        void HMI_main_loop();
+    private:
+        ExecutionSpecifications executionSpecifications;
+        void initialize_execution_specifications_struct();
+        GUI gui{};
+        Keyboard keyboard{};
+        void main_menu();
+        void define_execution_specifications();
+       //Functions to set up the execution specifications
+       //Funciones para configurar las especificaciones de ejecución
+        void select_places();
+       //
+};
---
+++ lib/Keyboard/keyboard.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <Keypad.h>
+#include <Arduino.h>
+#include <vector>
+#define ROWS  4 
+#define COLUMNS 4
+
+class Keyboard
+{
+    public:
+        Keyboard() = default;
+        char get_valid_key(std::vector<char>& validKeys);
+    private:
+        byte rowsPins[ROWS] = {4, 13, 14, 27}; 
+        byte columnsPins[COLUMNS] = {26, 25, 33, 32}; 
+        Keypad keypad = Keypad( makeKeymap(keys), rowsPins, columnsPins, ROWS, COLUMNS); 
+
+        char keys[ROWS][COLUMNS] = {
+            {'1','2','3','A'},
+            {'4','5','6','B'},
+            {'7','8','9','C'},
+            {'*','0','#','D'}
+        };
+};
+
+
