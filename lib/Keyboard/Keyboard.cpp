f2d76ec44dbf2bea8b8f9108f449a4f1a489f8b1
ArmandoIsaacParraCruz <armandoisaac.parracruz@gmail.com>
2023-07-06 12:27:17 -0600

    .

--- lib/Keyboard/Keyboard.cpp
+++ lib/Keyboard/Keyboard.cpp
@@ -3,15 +3,18 @@
 char Keyboard::get_valid_key(std::vector<char>& validKeys)
 {
     char keyPressed = NO_KEY;
+    bool valid = true;
     // Wait for a key press
     while (keyPressed == NO_KEY) {
         keyPressed = keypad.getKey();
         for(char& validKey: validKeys) {
-            if(keyPressed == validKey) {
-                break;
-            } else {
-                keyPressed ==  NO_KEY;
-            }
+            valid = valid && (keyPressed == validKey);
+        }
+
+        if(valid) {
+            break;
+        } else {
+            keyPressed == NO_KEY;
         }
     }
     return keyPressed;
